483482012fcc4950dad5c8e8e72f01390bcb22c0
Brad Bennett <bbennettjr@gmail.com>
2016-02-21 17:22:41 -0500

    Add comments to ObjectPositionAndColorData.m

--- Telekinesis/ObjectPositionAndColorData.m
+++ Telekinesis/ObjectPositionAndColorData.m
@@ -12,6 +12,7 @@
 @end
 
 @implementation ObjectPositionAndColorData
+    //static strings that make up the KEY values
 static NSString *const X_POSITION_KEY = @"X_POSITION_KEY";
 static NSString *const Y_POSITION_KEY = @"Y_POSITION_KEY";
 static NSString *const RED_KEY = @"RED_KEY";
@@ -19,9 +20,11 @@ static NSString *const GREEN_KEY = @"GREEN_KEY";
 static NSString *const BLUE_KEY = @"BLUE_KEY";
 
 #pragma mark - Initializer
+    //Designated initializer
 -(instancetype)initWithPositionsX:(float)x andY:(float)y andColorsRed:(float)red andGreen:(float)green andBlue:(float)blue{
     self = [super init];
     if (self) {
+            //if the object is created, wrap the given values in NSNumber format and send the pointers to the stack
         _xPosition = [[NSNumber alloc] initWithFloat:x];
         _yPosition = [[NSNumber alloc] initWithFloat:y];
         _red = [[NSNumber alloc] initWithFloat:red];
@@ -32,7 +35,8 @@ static NSString *const BLUE_KEY = @"BLUE_KEY";
 }
 
 #pragma mark - Instance Methods
-@synthesize propertyList = _propertyList; //create the setter and getter so must synchronize
+    //Create the setter and getter so must synchronize.  The property list getter will return an NSDictionary of the values initialized.  The setter allows the user to send a property list to the object, which parses out the correct KEY and adds the object to that KEY.  This is a fancy way of saying it keeps track of the data in one dictionary.
+@synthesize propertyList = _propertyList;
 -(NSDictionary *)propertyList{
     _propertyList = [[NSDictionary alloc] initWithObjects:self.allValues forKeys:[ObjectPositionAndColorData allKeys]];
     return _propertyList;
@@ -52,16 +56,20 @@ static NSString *const BLUE_KEY = @"BLUE_KEY";
     }
 }
 
+#pragma mark - Instance Methods
+    //Returns the value of each property in an NSArray
 -(NSArray *)allValues{
     return @[self.xPosition, self.yPosition, self.red, self.green, self.blue];
 }
 
+    //Return the current state of the data in string format
 -(NSString *)description{
     NSMutableString *descr = [NSMutableString stringWithFormat:@"X Position: %@ \nY Position: %@ \nRed: %@ \nGreen: %@ \nBlue: %@\n", self.xPosition, self.yPosition, self.red, self.green, self.blue];
     return descr;
 }
 
 #pragma mark - Class Methods
+    //Return all keys to the user
 +(NSArray *)allKeys{
     return @[X_POSITION_KEY, Y_POSITION_KEY, RED_KEY, GREEN_KEY, BLUE_KEY];
 }
