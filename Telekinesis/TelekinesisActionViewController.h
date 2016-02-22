ddcaf05a472417f31a3dca71da489f32c51f48ac
Brad Bennett <bbennettjr@gmail.com>
2016-02-21 20:02:37 -0500

    Add comments to TelekinesisActionViewController.h

--- Telekinesis/TelekinesisActionViewController.h
+++ Telekinesis/TelekinesisActionViewController.h
@@ -8,7 +8,10 @@
 
 #import <UIKit/UIKit.h>
 
+    //This view controller sets the stage for the focus of the app.  The user can meditate or focus on the central object and see how its color and position changes with time.
 @interface TelekinesisActionViewController : UIViewController
+    //The starting color of the center object, which currently is chosen from the button on the intial VC then sent over through a segue.  This functionality should change in the future as its clunky
 @property (nonatomic, strong) UIColor *startColor;
+    //This doesn't seem to be used, delete?
 @property (nonatomic) CGFloat number;
 @end
